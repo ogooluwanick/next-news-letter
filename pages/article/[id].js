dcbb0524d581b8acbf893f6c2d07fca710392ab2
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-11 01:27:25 +0100

    getStaticPaths

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -15,11 +15,29 @@ const index = ({article}) => {
   )
 }
 
-export const getServerSideProps= async (context)=>{
+// export const getServerSideProps= async (context)=>{
+//         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//         const article = await res.json()
+        
+//         return { props: {article} }
+// }
+export const getStaticProps= async (context)=>{
         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
         const article = await res.json()
         
         return { props: {article} }
 }
 
+export const getStaticPaths= async ()=>{
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+        const articles = await res.json()
+
+        const ids = articles.map((article) => article.id)
+        const paths = ids.map((id) => ({ params: { id: String(id) } }))
+
+        console.log(paths)
+
+        return { paths, fallback: false}
+}
 export default index
