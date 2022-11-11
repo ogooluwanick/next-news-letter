6b97e622a92009bfa1922ca23bcc5d3900a1518f
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-12 00:35:00 +0100

    Meta comp

--- components/Meta.js
+++ components/Meta.js
@@ -4,9 +4,20 @@ import React from 'react'
 const Meta = ({title,keywords,description}) => {
   return (
     <Head>
-        
+                <meta name='keywords' content={`${keywords}`} />
+                <meta name='description' content={description} />
+                <meta charSet='utf-8' />
+                <link rel='icon' href='logoIcon.png' />
+                <title>{`🌹O.G.O🌹 | ${title}`} </title>  
     </Head>
   )
+}
+
+Meta.defaultProps={
+        title:"News Letters",
+        keywords:"News, Web Dev, Next js, Ogooluwanick",
+        description:"Ogooluwanick's Next js News letters"
+
 }
 
 export default Meta
