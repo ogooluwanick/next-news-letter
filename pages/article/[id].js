ece948bb4f9f149472c70a031156a6ad5a5042ff
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-11 01:10:05 +0100

    getServerSideProps

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,14 +1,25 @@
+import Link from "next/link"
 import { useRouter } from "next/router"
 import React from 'react'
 
-const index = () => {
+const index = ({article}) => {
         // let route=useRouter()
         // let id=route.query.id
   return (
     <div>
-        this is article {id}
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br />
+        <Link href="/">Go Back&rarr;</Link>
     </div>
   )
+}
+
+export const getServerSideProps= async (context)=>{
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+        const article = await res.json()
+        
+        return { props: {article} }
 }
 
 export default index
