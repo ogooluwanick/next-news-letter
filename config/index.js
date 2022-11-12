91818876436b3d54d023c47c0e8f9ed6fc884d57
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-12 01:42:54 +0100

    config server

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const  dev=process.env.NODE_ENV!=="production"
 
-export const server =dev?"http://localhost:3001/":"https://ogo-news-letters.netlify.app/"
+export const server =dev?"http://localhost:3001":"https://ogo-news-letters.netlify.app"
