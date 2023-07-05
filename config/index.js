9760e706231d52fcd38dab3d9c74f90acca63807
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2023-07-05 11:23:16 +0100

    fix to codebase to account for vercel production lvl2

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const  dev=process.env.NODE_ENV!=="production"
 
-export const server =dev?"http://localhost:3002":"https://next-news-letter.vercel.app/"
+export const server =dev?"http://localhost:3002":"https://next-news-letter.vercel.app"
